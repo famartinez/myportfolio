e92189cfdba12260607757dca4797579b74fa7bd
“famartinez” <famartinez@gmal.com>
2023-04-17 14:20:17 -0700

    Edit README.md and hello_world.txt

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import About from "./components/About";
+
+
+export default function App() 
+{
+  return (
+    <main className="text-gray-400 bg-gray-900 body-font">
+
+      <About />
+    
+    </main>
+  );
+}
